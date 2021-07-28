156f2c1542aa20e5b2fa9e5a7bf06272ccb1526e
karczewskiPiotr <34404855+karczewskiPiotr@users.noreply.github.com>
2021-07-28 15:17:15 +0200

    Embed Hypothesis

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,3 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
+const { onRenderBody } = require("./config/gatsby-ssr")
 
-// You can delete this file if you're not using it
+exports.onRenderBody = onRenderBody
